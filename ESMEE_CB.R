336965b266400c51d06189991b5e126f2c457055
JPvdP <j.p.g.vanderpol@uu.nl>
2023-06-01 12:31:32 +0200

    Changed data loading

--- ESMEE_CB.R
+++ ESMEE_CB.R
@@ -1,3 +1,3 @@
 #  This script prepares the Crunchbase data to search for patents and publications
 
-ESMEE_CB_Energy_Agri_2010_2023_NL_31052023 <- read_csv("Presentation_062023/31052023_CB_Energy_Agri_2010_2023_NL.csv")
+load("~/Desktop/Projects/ESMEE/ESMEE_CB_Energy_Agri_2010_2023_NL_31052023.rdata")
